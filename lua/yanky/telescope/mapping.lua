3e205e9c5a6bb4cba9c90232ab150190b739461e
gbprod <contact@gb-prod.fr>
2022-06-27 08:56:37 +0200

    feat(telescope): introduce set register action
    Ref: #3

--- lua/yanky/telescope/mapping.lua
+++ lua/yanky/telescope/mapping.lua
@@ -1,4 +1,5 @@
 local picker = require("yanky.picker")
+local utils = require("yanky.utils")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
@@ -21,6 +22,14 @@ function mapping.delete()
   end
 end
 
+function mapping.set_register(register)
+  return function(prompt_bufnr)
+    actions.close(prompt_bufnr)
+    local selection = action_state.get_selected_entry()
+    picker.actions.set_register(register)(selection.value)
+  end
+end
+
 function mapping.get_defaults()
   return {
     default = mapping.put("p"),
@@ -28,11 +37,13 @@ function mapping.get_defaults()
       ["<c-p>"] = mapping.put("p"),
       ["<c-k>"] = mapping.put("P"),
       ["<c-x>"] = mapping.delete(),
+      ["<c-r>"] = mapping.set_register(utils.get_default_register()),
     },
     n = {
       p = mapping.put("p"),
       P = mapping.put("P"),
       d = mapping.delete(),
+      r = mapping.set_register(utils.get_default_register()),
     },
   }
 end
