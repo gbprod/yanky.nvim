c5694ed5c1676b29f02158e6eefadca0cdcc3472
gbprod <contact@gb-prod.fr>
2022-12-08 09:09:10 +0100

    feat(special-put): introduce Joined/Charwise/Blockwise
    Ref: #66

--- lua/yanky/wrappers.lua
+++ lua/yanky/wrappers.lua
@@ -17,6 +17,58 @@ function wrappers.linewise(next)
   end
 end
 
+function wrappers.charwise(next)
+  return function(state, callback)
+    local body = vim.fn.getreg(state.register)
+    local type = vim.fn.getregtype(state.register)
+
+    local reformated_body = body:gsub("\n$", "")
+    vim.fn.setreg(state.register, reformated_body, "c")
+
+    if nil == next then
+      callback(state)
+    else
+      next(state, callback)
+    end
+
+    vim.fn.setreg(state.register, body, type)
+  end
+end
+
+function wrappers.blockwise(next)
+  return function(state, callback)
+    local body = vim.fn.getreg(state.register)
+    local type = vim.fn.getregtype(state.register)
+
+    vim.fn.setreg(state.register, body, "b")
+
+    if nil == next then
+      callback(state)
+    else
+      next(state, callback)
+    end
+
+    vim.fn.setreg(state.register, body, type)
+  end
+end
+
+function wrappers.trim_and_join_lines(next)
+  return function(state, callback)
+    local body = vim.fn.getreg(state.register)
+
+    local reformated_body = body:gsub("%s*\r?\n%s*", " "):gsub("^%s*", ""):gsub("%s*$", "")
+    vim.fn.setreg(state.register, reformated_body, vim.fn.getregtype(state.register))
+
+    if nil == next then
+      callback(state)
+    else
+      next(state, callback)
+    end
+
+    vim.fn.setreg(state.register, body, vim.fn.getregtype(state.register))
+  end
+end
+
 function wrappers.change(change, next)
   return function(state, callback)
     if nil == next then
